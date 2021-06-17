927524fc38e33e879f8471c2b592d2436336be9f
liupengyu <1846214211@qq.com>
2021-06-17 17:39:06 +0800

    6.17-初次

---
+++ src/main/java/com/example/staffhouse/StaffhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.staffhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/staffhouse/StaffhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staffhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
