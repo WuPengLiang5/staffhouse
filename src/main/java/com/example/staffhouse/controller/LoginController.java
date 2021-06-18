5df9d3add632b0e18c33239511c3915370f87bf6
gaoyi <1784347301@qq.com>
2021-06-18 16:05:43 +0800

    6/18-高毅-更新3loginController(记得刷新依赖)

--- src/main/java/com/example/staffhouse/controller/LoginController.java
+++ src/main/java/com/example/staffhouse/controller/LoginController.java
@@ -1,8 +1,50 @@
 package com.example.staffhouse.controller;
 
+import com.example.staffhouse.entity.UserInfo;
+import com.example.staffhouse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * @author NIIT
  */
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public UserInfo login(HttpSession session, UserInfo userInfo){
+        UserInfo user = userService.getUserByLoginName(userInfo.getLoginName());
+        String rawPassword = userInfo.getPassword();
+        System.out.println(user.getLoginName());
+        String rightPassword = user.getPassword();
+        System.out.println(rightPassword);
+        if (rawPassword.equalsIgnoreCase(rightPassword)){
+            session.setAttribute("user", user);
+            return user;
+        }
+        else{
+            UserInfo user_not = new UserInfo();
+            user_not.setStatus(-1);
+            user_not.setUserName("notfound");
+            user_not.setLoginName("notfound");
+            return user_not;
+        }
+//        if (MD5Util.verify(rawPassword, rightPassword)){
+//            // 写入session 用户的id和用户的权限
+//            session.setAttribute("user", user);
+////            System.out.println("登录成功！");
+//            return user;
+//        }
+//        else{
+//            UserInfo user_not = new UserInfo();
+//            user_not.setStatus(-1);
+//            user_not.setUserName("notfound");
+//            user_not.setLoginName("notfound");
+//            return user_not;
+//        }
+    }
 }
