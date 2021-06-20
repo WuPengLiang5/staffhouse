37bd43e6daaee3c451485fb2901ea8ffca3831de
李中镜 <1046175474@qq.com>
2021-06-20 09:02:22 +0800

    6-20 NoticeController  李中镜

--- src/main/java/com/example/staffhouse/controller/NoticeController.java
+++ src/main/java/com/example/staffhouse/controller/NoticeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class NoticeController {
 
     @Autowired
-    NoticeService noticeService;
+    private NoticeService noticeService;
 
     @RequestMapping("/listNotice")
     public List<Notice> listNotice() {
