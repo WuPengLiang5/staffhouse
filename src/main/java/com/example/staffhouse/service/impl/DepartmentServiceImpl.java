b1be3d862238a95cc7a17786cbed4fe3025cb6ea
13364030851 <2233900674@qq.com>
2021-06-20 09:07:42 +0800

    6/20 黄涛ht

---
+++ src/main/java/com/example/staffhouse/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.staffhouse.service.impl;
+
+import com.example.staffhouse.dao.DepartmentDao;
+import com.example.staffhouse.entity.Department;
+import com.example.staffhouse.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @Override
+    public List<Department> listDepartment() {
+        return departmentDao.listDepartment();
+    }
+
+    @Override
+    public void saveDepartment(Department department) {
+        departmentDao.saveDepartment(department);
+    }
+
+    @Override
+    public void deleteDepartment(Integer departmentId) {
+        departmentDao.deleteDepartment(departmentId);
+    }
+
+    @Override
+    public void updateDepartment(Department department) {
+        departmentDao.updateDepartment(department);
+    }
+
+    @Override
+    public Department getDepartmentById(Integer departmentId) {
+        return departmentDao.getDepartmentById(departmentId);
+    }
+}
