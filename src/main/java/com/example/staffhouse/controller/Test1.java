32030e6aa57db118a2280279853d3ef271d8c474
WuPengLiang5 <2523157228@qq.com>
2021-06-20 11:34:54 +0800

    6/20 增加LoginControllerTest

--- src/main/java/com/example/staffhouse/controller/Test1.java
+++ src/main/java/com/example/staffhouse/controller/Test1.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class Test1 {
 
     @RequestMapping("/test")
     public String getTest() throws Exception {
