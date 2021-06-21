0aad6a90ba005d19d6f3f08d5a0d30a3e1b95607
liupengyu <1846214211@qq.com>
2021-06-21 11:22:31 +0800

    6.21 11:22 用户管理dao层和service层测试类

--- src/test/java/com/example/staffhouse/dao/UserDaoTest.java
+++ src/test/java/com/example/staffhouse/dao/UserDaoTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class UserDaoTest {
@@ -30,5 +32,52 @@ public class UserDaoTest {
         userInfo.setPassword("12");
 
         userDao.updateUserPassword(userInfo);
+    }
+
+    @Test
+    public void testListUserInfo(){
+        List<UserInfo> userList = userDao.listUserInfo();
+        JSONObject jsonObject=JSONObject.fromObject(userList);
+        System.out.println(jsonObject);
+    }
+
+    @Test
+    public void testListUserInfoByStatus(){
+        List<UserInfo> userList = userDao.listUserInfoByUserStatus(1);
+        JSONObject jsonObject=JSONObject.fromObject(userList);
+        System.out.println(jsonObject);
+    }
+
+    @Test
+    public void testGetUserInfoByUserName(){
+        String userName ="武朋亮";
+        List<UserInfo> userList = userDao.getUserInfoByUserName(userName);
+        JSONObject jsonObject=JSONObject.fromObject(userList);
+        System.out.println(jsonObject);
+    }
+
+    @Test
+    public void testUpdateUserInfoById(){
+        UserInfo userInfo=new UserInfo();
+        userInfo.setId(1);
+        userInfo.setLoginName("123");
+        userInfo.setStatus(1);
+        userInfo.setUserName("吴亮");
+        userDao.updateUserInfoById(userInfo);
+    }
+
+    @Test
+    public void testDeleteUserById(){
+        userDao.deleteUserById(1);
+    }
+
+    @Test
+    public void testSaveUserInfo() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setLoginName("123");
+        userInfo.setPassword("123456");
+        userInfo.setStatus(1);
+        userInfo.setUserName("陈佳明");
+        userDao.saveUserInfo(userInfo);
     }
 }
