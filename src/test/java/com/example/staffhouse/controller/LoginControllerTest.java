591a54fff3d72e96827abdfbafd8d9962bf53a88
WuPengLiang5 <2523157228@qq.com>
2021-06-22 18:30:02 +0800

    6/22 武朋亮 修改了LoginTest登录测试类

--- src/test/java/com/example/staffhouse/controller/LoginControllerTest.java
+++ src/test/java/com/example/staffhouse/controller/LoginControllerTest.java
@@ -1,14 +1,17 @@
 package com.example.staffhouse.controller;
 
 import com.example.staffhouse.entity.UserInfo;
+import com.example.staffhouse.util.FileUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import net.sf.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -54,11 +57,13 @@ public class LoginControllerTest {
             String requestJson=ow.writeValueAsString(userInfo);
             MvcResult mvcResult = mockMvc.perform(
                     MockMvcRequestBuilders.post("/login/doLogin")
-                            .contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                            .contentType(MediaType.APPLICATION_JSON).content(requestJson)
+                            .accept("application/json;charset=utf-8"))
                     .andExpect(MockMvcResultMatchers.status().isOk())
                     .andDo(MockMvcResultHandlers.print())
                     .andReturn();
-            System.out.println(mvcResult);
+            JSONObject jsonObject1=JSONObject.fromObject(mvcResult.getResponse().getContentAsString());
+            FileUtil.WriteStringToFile((String) jsonObject1.get("token"));
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -70,19 +75,26 @@ public class LoginControllerTest {
         try {
             Map<String,String> map=new HashMap<>();
             map.put("loginName","123");
-            map.put("password","12");
-            map.put("newPassword","12");
+            map.put("password","123");// 输入的原密码
+            map.put("newPassword","12");// 输入的新密码
+            String token = FileUtil.readTokenFile();
             //设置值
             ObjectMapper mapper = new ObjectMapper();
             ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
             String requestJson=ow.writeValueAsString(map);
             MvcResult mvcResult = mockMvc.perform(
-                    MockMvcRequestBuilders.post("/login/doLogin")
-                            .contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                    MockMvcRequestBuilders.post("/login/updateUserPassword")
+                            .contentType(MediaType.APPLICATION_JSON).content(requestJson)
+                            .header("token",token))
                     .andExpect(MockMvcResultMatchers.status().isOk())
                     .andDo(MockMvcResultHandlers.print())
                     .andReturn();
-            System.out.println(mvcResult);
+            if (mvcResult.getResponse().getContentAsString().equalsIgnoreCase("true")){
+                System.out.println("更改成功！");
+            }else{
+                System.out.println("与原密码不同，更改失败！");
+            }
+
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
